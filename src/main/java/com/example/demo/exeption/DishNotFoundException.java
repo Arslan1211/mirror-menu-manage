1096ea73b72bc6b0cdf7725ea82cb2916787d83c
Arslan1211 <Arslan1211@gmail.com>
2025-07-15 22:12:29 +0300

    добавлен Dockerfile, исправлена ошибка в обработке исключения по блюду.

--- src/main/java/com/example/demo/exeption/DishNotFoundException.java
+++ src/main/java/com/example/demo/exeption/DishNotFoundException.java
@@ -3,7 +3,7 @@ package com.example.demo.exeption;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class DishNotFoundException extends RuntimeException {
     public DishNotFoundException(Long dishId) {
         super(String.format("Dish not found with %d or you don't have permission", dishId));
