18b019641a4da7562e5cd48edc10b645df26578b
Arslan1211 <Arslan1211@gmail.com>
2025-07-25 16:08:26 +0300

    добавлена схема аутентификации

--- src/main/java/com/example/demo/Demo3Application.java
+++ src/main/java/com/example/demo/Demo3Application.java
@@ -1,9 +1,20 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+@OpenAPIDefinition(security = @SecurityRequirement(name = "Bearer Authentication"))
 public class Demo3Application {
 
     public static void main(String[] args) {
